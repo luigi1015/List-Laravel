e214dbbf8ec6ee2ec7fd03d46d78ec3389515196
Jeff <github@314ghz.com>
2016-04-04 21:12:26 -0500

    Added an initial Laravel app.

---
+++ list/app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace List\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ list/app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace List\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
