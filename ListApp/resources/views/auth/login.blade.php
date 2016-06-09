7ff2b4bb662dcfaa1658d2136a729c4f6f77d3a3
Jeff <github@314ghz.com>
2016-06-08 21:20:47 -0500

    Removed some login comments.

--- ListApp/resources/views/auth/login.blade.php
+++ ListApp/resources/views/auth/login.blade.php
@@ -8,12 +8,7 @@
 		<form method='POST' action='/login'>
 
 		{!! csrf_field() !!}
-<!--
-		<div>
-			<label for='email'>Email</label>
-			<input type='text' name='email' id='email' value="{{ old('email') }}">
-		</div>
--->
+
 		<div>
 			<label for='username'>Username</label>
 			<input type='text' name='username' id='username' value="{{ old('username') }}">
